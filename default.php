1613c59a5c3929f29f5a3ed0adb60d9b61d061b8
Leonardo Sato <leonardogsato@gmail.com>
2025-03-08 01:05:19 -0300

    add new CSS file for styling and update stylesheet link in default.php

--- default.php
+++ default.php
@@ -10,11 +10,11 @@
         <link rel="preconnect" href="https://fonts.googleapis.com">
         <link href="https://fonts.googleapis.com/css2?family=DM+Sans&display=swap" rel="stylesheet">
         <link href="https://fonts.googleapis.com/css2?family=DM+Sans:wght@400;700&display=swap" rel="stylesheet">
-        <link href="./css/main.css" rel="stylesheet">
+        <link href="./css/main_1.css" rel="stylesheet">
     </head>
     <body>
         <div id="loading-screen">
-            Loading...
+            <span id="loading-desc">Loading...</span>
             <div id="progress-bar-container">
                 <div id="progress-bar"></div>
             </div>
@@ -22,22 +22,34 @@
         <div id="headerinfo">
             <span class="nametag">LEONARDO SATO</span>
             <div class="link_icons">
-                <a href="https://www.linkedin.com/in/leonardo-gutierrez-sato/" class="icon" id="linkedin" target="_blank" title="Linkedin">
-                    <svg xmlns="http://www.w3.org/2000/svg" width="30px" height="30px" viewBox="0 0 16 16" fill="none">
-                        <path fill="#0A66C2" d="M12.225 12.225h-1.778V9.44c0-.664-.012-1.519-.925-1.519-.926 0-1.068.724-1.068 1.47v2.834H6.676V6.498h1.707v.783h.024c.348-.594.996-.95 1.684-.925 1.802 0 2.135 1.185 2.135 2.728l-.001 3.14zM4.67 5.715a1.037 1.037 0 01-1.032-1.031c0-.566.466-1.032 1.032-1.032.566 0 1.031.466 1.032 1.032 0 .566-.466 1.032-1.032 1.032zm.889 6.51h-1.78V6.498h1.78v5.727zM13.11 2H2.885A.88.88 0 002 2.866v10.268a.88.88 0 00.885.866h10.226a.882.882 0 00.889-.866V2.865a.88.88 0 00-.889-.864z"/>
-                    </svg>
-                </a>
-                <a href="https://github.com/satoLG/" class="icon" id="github" target="_blank" title="Github">
-                    <svg xmlns="http://www.w3.org/2000/svg" height="23px" width="23px" fill="#000000" version="1.1" id="Layer_1" viewBox="-143 145 512 512" xml:space="preserve">
-                        <g>
-                            <path d="M177.1,398.4c-8.6-1.5-17.6-1.3-26.4-0.8c-20,1.1-40,3-60,1.2c-9.5-0.9-19.1-1.8-28.7-1.6c-17.4,0.3-32,6-40.6,22.6   c-4.3,8.2-5.3,17-5,26.1c0.7,25.6,11.8,40.2,36.2,47.5c19.6,5.8,39.7,6.6,59.9,6.2c7.5,0,15,0.4,22.5-0.1   c15.5-0.9,30.7-3.2,45.4-8.6c15.2-5.6,24.3-16.3,27.6-31.8c1.3-6,1.9-12.3,1.8-18.4C209.6,420,195.6,401.5,177.1,398.4z    M78.7,466.1c-6.5,7.1-15.9,7.2-22.6,0.3c-4.9-5-7.7-12.7-7.7-22.3c0.2-6.5,2.1-13.6,7.7-19.3c6.7-6.9,16.1-6.8,22.6,0.2   C88.5,435.6,88.5,455.5,78.7,466.1z M169.6,466.6c-6.1,6.3-14.9,6.5-21.4,0.7c-11.2-10.2-11.2-32.9,0-43.2   c6.4-5.9,15.2-5.7,21.4,0.6c5.7,5.8,7.6,13.1,7.9,20.9C177.2,453.5,175.2,460.7,169.6,466.6z"/>
-                            <path d="M329,145h-432c-22.1,0-40,17.9-40,40v432c0,22.1,17.9,40,40,40h432c22.1,0,40-17.9,40-40V185C369,162.9,351.1,145,329,145z    M244.2,423.4c-1.4,11.4-3.8,23.1-7.9,33.8c-12,30.7-36,47.6-67.8,52.7c-18.2,2.9-36.9,3-57.1,4.5c-18.1-1.6-38-1.8-57.3-5.2   c-37.4-6.6-62.8-32.8-70.2-70.3c-3.8-19.1-4.9-38.3,1-57.3c3.1-9.8,8.2-18.5,14.8-26.4c0.9-1,1.7-2.5,1.6-3.8   c-1.1-17.2,0.9-34.2,6-50.6c4.2-13.7,1.1-12.9,16.3-8.9c18.3,4.8,34.3,14.7,50,25c1.8,1.2,4.6,1.7,6.8,1.3   c22.2-3.4,44.3-3.6,66.5,0.3c1.6,0.3,3.7-0.3,5.2-1.2c13.5-8.8,27.4-16.7,42.6-22.2c5.5-2,11.3-3.3,16.9-5c2.5-0.7,3.6,0.2,4.5,2.6   c6.8,19,9.6,38.5,8.6,58.6c-0.1,1.1,0.5,2.6,1.2,3.5C243,374.6,247.3,398.2,244.2,423.4z"/>
+                <a href="https://www.linkedin.com/in/leonardo-gutierrez-sato/" class="icon" id="linkedin" target="_blank" title="Linkedin">    
+                    <svg xmlns="http://www.w3.org/2000/svg" height="25px" width="25px" aria-label="LinkedIn" role="img" viewBox="0 0 512 512" fill="#ffffff" stroke="#ffffff">
+                        <g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g>
+                        <g id="SVGRepo_iconCarrier">
+                            <rect width="512" height="512" rx="15%" fill="#0077b5"></rect>
+                            <circle cx="142" cy="138" r="37"></circle>
+                            <path stroke="#ffffff" stroke-width="66" d="M244 194v198M142 194v198"></path>
+                            <path d="M276 282c0-20 13-40 36-40 24 0 33 18 33 45v105h66V279c0-61-32-89-76-89-34 0-51 19-59 32">
+
+                            </path>
                         </g>
                     </svg>
                 </a>
+                <a href="https://github.com/satoLG/" class="icon" id="github" target="_blank" title="Github">
+                    <svg xmlns="http://www.w3.org/2000/svg" aria-label="GitHub" role="img" viewBox="0 0 512 512" height="25px" width="25px" fill="#000000"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"><rect width="512" height="512" rx="15%" fill="#1B1817"></rect><path fill="#ffffff" d="M335 499c14 0 12 17 12 17H165s-2-17 12-17c13 0 16-6 16-12l-1-50c-71 16-86-28-86-28-12-30-28-37-28-37-24-16 1-16 1-16 26 2 40 26 40 26 22 39 59 28 74 22 2-17 9-28 16-35-57-6-116-28-116-126 0-28 10-51 26-69-3-6-11-32 3-67 0 0 21-7 70 26 42-12 86-12 128 0 49-33 70-26 70-26 14 35 6 61 3 67 16 18 26 41 26 69 0 98-60 120-117 126 10 8 18 24 18 48l-1 70c0 6 3 12 16 12z"></path></g></svg>                    
+                </a>
                 <a href="https://codepen.io/satoLG/" class="icon" id="codepen" target="_blank" title="Codepen">
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="#000000" width="30px" height="30px" viewBox="-2 -2 24 24" preserveAspectRatio="xMinYMin" class="jam jam-codepen-square">
-                        <path d="M10.402 5.131a.79.79 0 0 0-.887.01L5.54 7.91a.79.79 0 0 0-.339.648v2.982a.79.79 0 0 0 .355.659l4.039 2.67a.79.79 0 0 0 .87 0l4.12-2.722a.79.79 0 0 0 .354-.658v-2.93a.79.79 0 0 0-.354-.66l-4.184-2.768zm3.077 3.23l-1.523 1.023-1.428-1.038-.009-2.043 2.96 2.059zM9.486 6.306v1.999L8.023 9.327l-1.46-.979 2.923-2.043zM6.164 9.333l.944.633-.944.661V9.333zm3.347 4.261l-2.947-1.976 1.472-1.029 1.475.99v2.015zm-.56-3.644l.955-.667 1.11.73-.984.662-1.08-.725zm1.601 3.644v-2.016l1.41-.944 1.517.997-2.927 1.963zm3.347-2.933l-1-.657 1-.671v1.328z"/><path d="M4 2a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V4a2 2 0 0 0-2-2H4zm0-2h12a4 4 0 0 1 4 4v12a4 4 0 0 1-4 4H4a4 4 0 0 1-4-4V4a4 4 0 0 1 4-4z"/>
+                    <svg xmlns="http://www.w3.org/2000/svg" height="25px" width="25px" aria-label="CodePen" role="img" viewBox="0 0 512 512" fill="#000000">
+                        <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
+                        <g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g>
+                        <g id="SVGRepo_iconCarrier"> 
+                            <rect width="512" height="512" rx="15%" fill="#111"></rect> 
+                            <g fill="none" stroke="#ffffff" stroke-width="33" stroke-linejoin="round"> 
+                                <path d="M81 198v116l175 117 175-117V198L256 81z"></path> 
+                                <path d="M81 198l175 116 175-116M256 81v117"></path> <path d="M81 314l175-116 175 116M256 431V314">
+                                </path> 
+                        </g> 
+                        </g>
                     </svg>
                 </a>
             </div>    
