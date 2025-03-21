dc6511006e4cfff9feb19b775f541a762bdb0d0e
Leonardo Sato <leonardogsato@gmail.com>
2025-03-21 00:53:33 -0300

    threejs version fix

--- default.php
+++ default.php
@@ -72,8 +72,8 @@
 		<script type="importmap">
 			{
 				"imports": {
-					"three": "https://unpkg.com/three@0.174.0/build/three.module.js",
-					"three/addons/": "https://app.unpkg.com/three@0.174.0/files/examples/jsm/"
+					"three": "https://unpkg.com/three@0.150.0/build/three.module.js",
+					"three/addons/": "https://unpkg.com/three@0.150.0/examples/jsm/"
 				}
 			}
 		</script>
